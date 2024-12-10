2fb50688ed4b8e4a7f797914ab7004d41235cff2
Jeung Byeong Guk <zpsvkf@naver.com>
2024-12-10 18:22:43 +0900

    24.12.10 - 정병국
    추가된 부분
    -
    
    수정된 부분
    HomeController.java - 타임리프 테스트요 더미데이터 추가
    auth-index.html - 타임리프 테스트
    
    기타사항
    -

--- pcShoppingMall/src/main/java/com/project/easyBuild/controller/HomeController.java
+++ pcShoppingMall/src/main/java/com/project/easyBuild/controller/HomeController.java
@@ -1,7 +1,17 @@
 package com.project.easyBuild.controller;
 
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.project.easyBuild.product.biz.ProductBiz;
+import com.project.easyBuild.product.dao.ProductDao;
+import com.project.easyBuild.product.dto.ProductDto;
 
 @Controller
 public class HomeController {
@@ -15,8 +25,17 @@ public class HomeController {
     	return "example/example";
     }
     
+	@Autowired
+	private ProductBiz productbiz;
+	
+    
     @GetMapping("/auth-index")
-    public String authIndex() {
+    public String authIndex(Model model) {
+    	
+		List<ProductDto> res = productbiz.listAll();
+		res.add(new ProductDto(1, "유저1", 1, 1, 1, "제품1", 1, new Date()));
+		model.addAttribute("list",res);
+
     	return "pages/authority/auth-index";
     }
 }
