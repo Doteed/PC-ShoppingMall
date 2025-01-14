f48356a9d60d166cd11ede92e0bc31e28aa4a400
Kim Ki Hyeoun <5739kkh@naver.com>
2025-01-14 14:27:43 +0900

    25.01.14 - 김기현
    수정된 내용
    caseproducts.html - 로그인 수정
    casecontroller - 로그인 수정
    caseservice - 로그인 수정
    style.css - 메뉴바 높이 수정
    floating.js - 로그인 수정
    productdetail.js - 로그인 수정
    header.html - pc부품 링크 수정

--- pcShoppingMall/src/main/resources/static/js/product/productDetails.js
+++ pcShoppingMall/src/main/resources/static/js/product/productDetails.js
@@ -4,7 +4,12 @@ document.addEventListener('DOMContentLoaded', () => {
     const quantityInput = document.getElementById('quantity');
     const totalPriceElement = document.getElementById('total-price');
 	const tabs = document.querySelectorAll('.tab');
-
+	// 로그인 상태 확인
+	const isLoggedIn = document.body.dataset.loggedIn === "true";
+	// 장바구니 및 구매하기 버튼
+	const addToCartButton = document.getElementById('add-to-cart');
+	const buyNowButton = document.getElementById('buy-now');
+	
     // 가격 정보 가져오기
     const unitPrice = parseInt(totalPriceElement.textContent.replace(/[^\d]/g, ''), 10);
 
@@ -40,18 +45,30 @@ document.addEventListener('DOMContentLoaded', () => {
         updateTotalPrice();
     });
 
-    // "장바구니" 버튼 클릭 이벤트
-    document.getElementById('add-to-cart').addEventListener('click', () => {
-        alert('장바구니에 추가되었습니다.');
-        // 장바구니 추가 로직을 여기에 구현
-    });
+	// 버튼 클릭 이벤트 핸들러
+	const handleButtonClick = (event, action) => {
+		if (!isLoggedIn) {
+		   // 로그인 필요 얼트 메시지
+		   const userAction = confirm("로그인이 필요합니다. 로그인하시겠습니까?");
+		   if (userAction) {
+		       window.location.href = '/loginform'; // 로그인 페이지로 이동
+		    }
+		    return; // 동작 중단
+		 }
 
-    // "구매하기" 버튼 클릭 이벤트
-    document.getElementById('buy-now').addEventListener('click', () => {
-        alert('구매 페이지로 이동합니다.');
-        // 구매하기 로직을 여기에 구현
-        //window.location.href = '/purchase'; // 구매 페이지로 이동 (예시 URL)
-    });
+		 if (action === 'add-to-cart') {
+		     const userAction = confirm("장바구니에 담았습니다. 장바구니로 이동 하시겠습니까?");
+		     if (userAction) {
+		         window.location.href = '/cart'; // 장바구니 페이지로 이동
+		     }
+		 } else if (action === 'buy-now') {
+		     window.location.href = '/buynow'; // 구매 페이지로 이동
+		 }
+	};
+
+	// 이벤트 리스너 연결
+	addToCartButton.addEventListener('click', (e) => handleButtonClick(e, 'add-to-cart'));
+	buyNowButton.addEventListener('click', (e) => handleButtonClick(e, 'buy-now'));
 	
 	// 탭 클릭 시 스크롤 이벤트
 	    tabs.forEach(tab => {
